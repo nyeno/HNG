ddf36a48264e4b5cbd395727c6360f55b333cc89
Laura Eno <enonyenooke@gmail.com>
2022-10-28 02:20:58 +0100

    Task Complete

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -21,19 +21,19 @@ function App() {
       <a href="https://twitter.com/_laurae" id="twitter" target="_blank" rel="noreferrer">
           Twitter Link
         </a>
-        <a href="https://training.zuri.team/" id="btn_zuri" target="_blank" rel="noreferrer">
+        <a href="https://training.zuri.team/" id="btn__zuri" target="_blank" rel="noreferrer">
           Zuri Team
         </a>
         <a href="http://books.zuri.team/" id="books" target="_blank" rel="noreferrer">
           Zuri Books
         </a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=nyenooke" id="boook_python" target="_blank" rel="noreferrer">
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=nyenooke" id="book__python" target="_blank" rel="noreferrer">
           Python Books
         </a>
         <a href="https://background.zuri.team/" id="pitch" target="_blank" rel="noreferrer">
           Background Checks for coders
         </a>
-        <a href="https://books.zuri.team/design-rules" id="book_design" target="_blank" rel="noreferrer">
+        <a href="https://books.zuri.team/design-rules" id="book__design" target="_blank" rel="noreferrer">
           Design Books
         </a>
         <div className="socials" id="">
