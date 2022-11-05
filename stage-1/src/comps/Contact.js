9fb841e7f00496124c3107a20ad8dca685a031ca
Laura Eno <enonyenooke@gmail.com>
2022-11-05 05:25:45 +0100

    feat: Added styling for form

--- stage-1/src/comps/Contact.js
+++ stage-1/src/comps/Contact.js
@@ -3,7 +3,7 @@ import './Contact.css'
 
 export default function Contact() {
   const name = "Nyenooke Eno"
-  const [focused, setFocused] = useState(false);
+ // const [focused, setFocused] = useState(false);
   const [blur, setBlur] = useState({
     first: false,
     last: false,
@@ -14,7 +14,7 @@ export default function Contact() {
   });
 
   const handleFocus = (e) => {
-    setFocused(true);
+    //setFocused(true);
     setBlur({ ...blur, [e.target.name]: true });
     //console.log(blur)
   };
@@ -25,37 +25,34 @@ export default function Contact() {
         <h2>
             Contact Me
         </h2>
-        <h3>Hi there, contact me to ask me about anything you have in mind.</h3>
-        <form>
-            <div className='names'>
-                <div>
-                    <label>First Name</label>
-                    <input id="first_name" name='first' type="text" required placeholder='Enter your first name' onBlur={handleFocus} focused={blur['first'].toString()}/>
-                    <p>Please enter your first name</p>
-                </div>
-                <div>
-                    <label>Last Name</label>
-                    <input id="last_name" name='last' type="text" required placeholder='Enter your last name' onBlur={handleFocus} focused={blur['last'].toString()}/>
-                    <p>Please enter your last name</p>
-                </div>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        <form className='form'>
+            <div className='first'>
+                <label>First Name</label>
+                <input id="first_name" name='first' type="text" required placeholder='Enter your first name' onBlur={handleFocus} focused={blur['first'].toString()}/>
+                <p className='error-message'>Please enter your first name</p>
+            </div>
+            <div className='last'>
+                <label>Last Name</label>
+                <input id="last_name" name='last' type="text" required placeholder='Enter your last name' onBlur={handleFocus} focused={blur['last'].toString()}/>
+                <p className='error-message'>Please enter your last name</p>
             </div>
             <div>
                 <label>Email</label>
                 <input id="email" name='email' type="email" required placeholder='yourname@gmail.com' onBlur={handleFocus} focused={blur['email'].toString()}/>
-                <p>Please provide a valid email address</p>
+                <p className='error-message'>Please provide a valid email address</p>
             </div>
             <div>
                 <label>Message</label>
-                <textarea id="message" name='message' required placeholder="Send me a message and I'll reply you as soon as possible" onBlur={handleFocus} onFocus={handleFocus} focused={focused.toString()}/>
-                <p>Please enter a message</p>  
+                <textarea id="message" name='message' required placeholder="Send me a message and I'll reply you as soon as possible..." onBlur={handleFocus} focused={blur['message'].toString()}/>
+                <p className='error-message'>Please enter a message</p>  
                 
             </div>
-            <div>
+            <div className='check'>
                 <input type="checkbox" name='checked' required/>
                 <label>You agree to sending your data to {name} who may contact you.</label>
-                <p>Please check the box</p>
             </div>
-            <button id="btn__submit">Send Message</button>
+            <button id="btn__submit" type='submit'>Send Message</button>
         </form>
     </div>
   )
--- stage-1/src/comps/Footer.js
+++ stage-1/src/comps/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
   return (
     <footer className="footer" id="">
         <img src={vector} id="vector" alt="logo"/>  
-        <p>HNG Internship 9 Frontend Task</p>
+        <p className='fp'>HNG Internship 9 Frontend Task</p>
         <img src={i4g} id="i4g" alt="logo"/> 
     </footer>
   )
