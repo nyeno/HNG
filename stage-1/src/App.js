faaa7fea73b15be2fc8632125fe2c01e582bf485
Laura Eno <enonyenooke@gmail.com>
2022-11-05 05:47:28 +0100

    refactor: Correcting errors with deployment

--- stage-1/src/App.js
+++ stage-1/src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 import Contact from './comps/Contact';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Home from './comps/Home';
 import Footer from './comps/Footer';
 
